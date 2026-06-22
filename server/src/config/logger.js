9d444177cae5bcfa4b63c232e2c95d3a780163a7
praful-koli <prafulkoli852@gmail.com>
2026-06-22 18:25:13 +0530

    fix: improve logger configuration to handle missing LOGGER_LEVEL gracefully

--- server/src/config/logger.js
+++ server/src/config/logger.js
@@ -1,9 +1,9 @@
 import pino from "pino";
-import env from "./env.js"
+import env from "./env.js";
 
 export default pino({
-  level: env.LOGGER_LEVEL,
+  level: env?.LOGGER_LEVEL || process.env.LOGGER_LEVEL || "info",
   transport: {
     target: "pino-pretty",
-  }
-})
+  },
+});
